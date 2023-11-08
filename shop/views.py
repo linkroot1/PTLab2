cb265941a6c7e4e777385534bc55b8a09770f045
linkroot1 <lekha-now@mail.ru>
2023-11-08 20:03:18 +0300

    WIP

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
+from django.db.models import F
+
 
 from .models import Product, Purchase
 
 # Create your views here.
 def index(request):
+    #ToDo: тут надо добавить еще условие что если у записей поле discount false то применяем скидку и меняем значение поля объектов на true
+    if Purchase.objects.all().count() == 2 and Purchase.objects.get(id=1).discount == 0:
+        Product.objects.all().update(price = F("price") * 0.6)
+
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'shop/index.html', context)
